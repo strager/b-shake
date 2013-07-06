653a81a461ea0b0f098352ecb39ff8d8786e5297
Matt Glazar <matt.glazar@gmail.com>
2013-07-06 18:23:10 -0500

    Use b's evalBuildStringPath

--- B/Shake/Core/Run.hs
+++ B/Shake/Core/Run.hs
@@ -6,33 +6,21 @@ module B.Shake.Core.Run
   ( run
   ) where
 
-import Control.Concurrent.MVar
-import Control.Exception (throwIO)
 import Data.Binary (Binary)
 import Data.Semigroup
 import Data.Typeable
-import Data.Typeable.Internal (TypeRep(TypeRep))
-import System.Directory
 
 import qualified Data.Binary as Binary
-import qualified Data.Binary.Get as Binary
-import qualified Data.Binary.Put as Binary
-import qualified Data.ByteString.Lazy as BSLazy
 
 import B.Build
-import B.Monad
-import B.Oracle.Binary
 import B.Question
-import B.RuleDatabase (RuleDatabase)
 import B.Shake.Core.Action
 import B.Shake.Core.Action.Internal (toBuildRule)
 import B.Shake.Core.Rules
 import B.Shake.Core.Rules.Internal (runRules)
 import B.Shake.Types (ShakeOptions)
+import B.Simple (evalBuildStringPath)
 
-import qualified B.Oracle as Oracle
-import qualified B.Oracle.InMemory as InMemory
-import qualified B.Oracle.InMemory.Pure as OraclePure
 import qualified B.Rule as B
 import qualified B.RuleDatabase as RuleDatabase
 
@@ -40,7 +28,7 @@ run :: ShakeOptions -> Rules () -> IO ()
 run _ rules = do
   (ruleDatabase, actions) <- runRules rules
   let ruleDatabase' = RuleDatabase.insert FooR ruleDatabase
-  go "build.db" ruleDatabase' (build (FooQ actions))
+  evalBuildStringPath "build.db" ruleDatabase' (build (FooQ actions))
 
 data FooQ = FooQ [Action ()]
   deriving (Typeable)
@@ -70,49 +58,3 @@ instance B.Rule FooQ FooR where
 
 instance Semigroup FooR where
   FooR <> FooR = FooR
-
-go
-  :: FilePath
-  -> RuleDatabase IO
-  -> Build IO a
-  -> IO a
-go dbPath ruleDatabase m = do
-  exists <- doesFileExist dbPath
-  state <- if exists
-    then readOracle dbPath
-    else return OraclePure.empty
-
-  storage <- newMVar state
-  let oracle = InMemory.mkMVarOracleWithStorage storage
-  Oracle.recheckAll oracle
-
-  mResult <- runBuild ruleDatabase oracle logMessage m
-
-  state' <- takeMVar storage
-  writeOracle dbPath state'
-
-  either (throwIO . head) return mResult
-
-  where
-  logMessage :: (Show a) => a -> IO ()
-  logMessage x = putStrLn ("> " ++ show x)
-
-  lookupQuestion
-    :: Fingerprint
-    -> Maybe (AQuestion IO{-undefined-})
-  lookupQuestion fingerprint = RuleDatabase.lookupQuestion
-    (TypeRep fingerprint undefined undefined)
-    ruleDatabase
-
-  readOracle
-    :: FilePath
-    -> IO (OraclePure.State IO)
-  readOracle path = decode `fmap` BSLazy.readFile path
-    where
-    decode = Binary.runGet (OraclePure.getState lookupQuestion)
-
-  writeOracle :: FilePath -> OraclePure.State IO -> IO ()
-  writeOracle path
-    = BSLazy.writeFile path
-    . Binary.runPut
-    . OraclePure.putState
